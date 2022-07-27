7cde845a7d628127f548199c9d7cc348a2acecbe
Lydia Bell <lydiabell@Lydias-MacBook-Pro.local>
2022-07-27 15:30:19 -0500

    Added Buttons and Input Text Boxes

---
+++ script.js
@@ -0,0 +1,23 @@
+const calc = () => {
+    let D = "";
+    let A = document.getElementById("num1").value;
+    let B = document.getElementById("op").value;
+    let C = document.getElementById("num2").value;
+    if(B == "+")
+    {
+        D = A+C;
+    }
+    elseif(B == "-")
+    {
+        D = A-C;
+    }
+    elseif(B == "*")
+    {
+        D = A*C;
+    }
+    elseif(B == "÷")
+    {
+        D = A/C;
+    }
+    document.getElementById("result").innerHTML = D;
+}
