3d60d07b83b5b2678237e5b26a4670d912fa2d35
Lydia Bell <lydiabell@Lydias-MacBook-Pro.local>
2022-07-27 16:39:20 -0500

    Added Some CSS Styling

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-const calc = () => {
-    let D = "";
-    let A = document.getElementById("num1").value;
-    let B = document.getElementById("op").value;
-    let C = document.getElementById("num2").value;
-    if(B == "+")
-    {
-        D = A+C;
-    }
-    elseif(B == "-")
-    {
-        D = A-C;
-    }
-    elseif(B == "*")
-    {
-        D = A*C;
-    }
-    elseif(B == "÷")
-    {
-        D = A/C;
-    }
-    document.getElementById("result").innerHTML = D;
-}
+
+
+
+// const calc = () => {
+//     let D = "";
+//     let A = document.getElementById("num1").value;
+//     let B = document.getElementById("op").value;
+//     let C = document.getElementById("num2").value;
+//     if(B == "+")
+//     {
+//         D = A+C;
+//     }
+//     elseif(B == "-")
+//     {
+//         D = A-C;
+//     }
+//     elseif(B == "*")
+//     {
+//         D = A*C;
+//     }
+//     elseif(B == "÷")
+//     {
+//         D = A/C;
+//     }
+//     document.getElementById("result").innerHTML = D;
+// }
